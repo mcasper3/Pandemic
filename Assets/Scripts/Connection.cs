034f9ecede2b2e952f72c7782b6a803a0fe4f427
Mike Casper <Mikecasper395@gmail.com>
2017-07-13 19:19:37 -0500

    updated board

--- Assets/Scripts/Connection.cs
+++ Assets/Scripts/Connection.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Connection : MonoBehaviour {
@@ -20,6 +21,8 @@ public class Connection : MonoBehaviour {
         else
         {
             UpdatePlayerList();
+
+            SceneManager.LoadScene("Game");
         }
     }
 
---
+++ Assets/Scripts/GameCounters.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCounters : MonoBehaviour {
+	
+    // TODO remove this method
+	public void OnEpidemicCountClicked()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+
+        Application.Quit();
+    }
+}
