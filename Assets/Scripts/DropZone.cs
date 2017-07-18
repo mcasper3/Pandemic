1fd557ca4bf1167157bd9183711d29e20b958eb6
Mike Casper <Mikecasper395@gmail.com>
2017-07-18 09:52:41 -0500

    finished basic game

--- Assets/Scripts/DropZone.cs
+++ Assets/Scripts/DropZone.cs
@@ -13,6 +13,7 @@ public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPoin
 
     public List<GameObject> usedCards;
     private int usedCardCount;
+    private GameObject usedCardDisplay;
 
     public void OnPointerExit(PointerEventData eventData)
     {
@@ -100,9 +101,56 @@ public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPoin
 
     public void OnClick()
     {
-        if (this.transform.childCount > 2)
+        if (usedCardDisplay == null)
         {
-            // TODO show all cards in the usedCards list
+            if (this.transform.childCount > 2)
+            {
+                // TODO show all cards in the usedCards list
+                usedCardDisplay = new GameObject();
+                Image image = usedCardDisplay.AddComponent<Image>();
+                image.color = new Color(73, 12, 94);
+
+                RectTransform rect = usedCardDisplay.GetComponent<RectTransform>();
+                rect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 500);
+                rect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 900);
+
+                var gridLayout = usedCardDisplay.AddComponent<GridLayoutGroup>();
+                gridLayout.cellSize = new Vector2(56, 83);
+                gridLayout.childAlignment = TextAnchor.MiddleCenter;
+                gridLayout.constraint = GridLayoutGroup.Constraint.FixedColumnCount;
+                gridLayout.constraintCount = 16;
+
+                usedCardDisplay.transform.SetParent(this.transform.parent);
+                usedCardDisplay.transform.position = this.transform.parent.position;
+
+                foreach (var card in usedCards)
+                {
+                    card.transform.SetParent(usedCardDisplay.transform);
+                    card.GetComponent<CanvasGroup>().blocksRaycasts = true;
+                }
+
+                usedCards.Clear();
+            }
+        }
+        else
+        {
+            int numCards = usedCardDisplay.transform.childCount;
+
+            for (int i = 0; i < numCards; i++)
+            {
+                GameObject card = usedCardDisplay.transform.GetChild(0).gameObject;
+                card.transform.SetParent(this.transform);
+                card.transform.SetSiblingIndex(card.transform.GetSiblingIndex() - 2);
+                card.transform.localPosition = new Vector3(28, 0);
+                card.GetComponent<CanvasGroup>().blocksRaycasts = false;
+                usedCards.Add(card);
+            }
+
+            usedCardCount = usedCards.Count;
+            usedCardCounter.text = usedCardCount.ToString();
+
+            Destroy(usedCardDisplay);
+            usedCardDisplay = null;
         }
     }
 
